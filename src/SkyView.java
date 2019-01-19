d1082853302f8f30dfdac2be6b2b380a5b49afe8
wchen1325 <wchen1325@bths.edu>
2019-01-18 20:56:26 -0500

    done

--- src/SkyView.java
+++ src/SkyView.java
@@ -5,18 +5,26 @@ public class SkyView {
         view = new double[numRows][numCols];
         int index = 0;
         for(int i = 0; i< numRows; i++){
-            for(int j = 0; j<numCols;j++){
-                view[i][j]=scanned[index];
-                index++;
+            if(i%2 == 0) {
+                for (int j = 0; j < numCols; j++) {
+                    view[i][j] = scanned[index];
+                    index++;
+                }
+            }
+            else{
+                for (int j = numCols-1; j >= 0; j--) {
+                    view[i][j] = scanned[index];
+                    index++;
+                }
             }
         }
     }
 
     public double getAverage(int startRow, int endRow, int startCol, int endCol){
-        double sum=0;
-        int count = 0;
-        for(int i = startRow; i < endRow; i++){
-            for(int j=startCol; j<endCol;j++){
+        double sum = 0;
+        double count = 0;
+        for(int i = startRow; i <= endRow; i++){
+            for(int j=startCol; j <= endCol;j++){
                 sum=sum+view[i][j];
                 count++;
             }
@@ -28,7 +36,7 @@ public class SkyView {
         String str="";
         for(int i = 0; i< view.length; i++) {
             for (int j = 0; j < view[i].length; j++) {
-                str = str + view[i][j] + ", ";
+                    str = str + view[i][j] + ", ";
             }
             str = str +"\n";
         }
